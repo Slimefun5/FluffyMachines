008f473f1d737d13bb19def43add2d36df92c255
Intisy <finn@birich.de>
2026-04-24 21:59:11 +0200

    Add JUnit 5 and MockBukkit test scaffolding

---
+++ src/test/java/io/ncbpfluffybear/fluffymachines/FluffyMachinesTest.java
@@ -0,0 +1,16 @@
+package io.ncbpfluffybear.fluffymachines;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Basic test scaffolding for FluffyMachines.
+ */
+class FluffyMachinesTest {
+
+    @Test
+    void classLoads() {
+        assertNotNull(FluffyMachines.class);
+    }
+}
