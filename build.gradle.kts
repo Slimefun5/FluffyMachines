9625a90a21362a6f68cb10d7c342e78785c2319c
intisy (Finn Birich) <finn@birich.de>
2026-09-23 11:23:59 +0200

    chore: point ci and build at development and main

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 group = "io.ncbpfluffybear"
 description = "FluffyMachines is a Slimefun addon that adds various machines, tools, and utilities."
 
-apply(from = "https://raw.githubusercontent.com/Slimefun5/gradle/stable/slimefun-addon.gradle")
+apply(from = "https://raw.githubusercontent.com/Slimefun5/gradle/main/slimefun-addon.gradle")
 
 dependencies {
     githubImplementation("Slimefun5:SlimefunMetrics:v1.0.0")
